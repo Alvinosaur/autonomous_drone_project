4b26cc21ca88b980ee539ecef0a262e6824cdb3b
Alvin <ashek@andrew.cmu.edu>
2019-03-05 03:27:00 -0500

    functional global pose of drone

--- auton_drone/src/plot_drone.cpp
+++ auton_drone/src/plot_drone.cpp
@@ -2,35 +2,35 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 
-tf::Vector3 get_cam_pose(tf::TransformListener tl){
-  tf::Vector3 tag0 = tf::Vector3(0.2, 0.2, 0);
-  tf::Vector3 tag1 = tf::Vector3(0.2, 0.7588, 0);
+tf::Transform get_cam_pose(const tf::TransformListener &tl, tf::Transform &tag0, tf::Transform &tag1){
+  tf::StampedTransform tag0_cam;
+  tf::StampedTransform tag1_cam;
+  tf::Transform cam_tag0;
+  tf::Transform cam_tag1;
+  tf::Vector3 cam_tag0_vec;
 
   // Need check to see if timestamp is too old since don't want to use old
   // transform if lost track of tag for a while
-  tl.waitForTransform("camera", "tag_0", ros::Time(0), ros::Duration(10.0));
-  tl.waitForTransform("camera", "tag_1", ros::Time(0), ros::Duration(10.0));
+  tl.waitForTransform("camera_raw", "tag_0", ros::Time(0), ros::Duration(10.0));
+  tl.waitForTransform("camera_raw", "tag_1", ros::Time(0), ros::Duration(10.0));
 
-  tf::StampedTransform tag0_cam;
-  tf::StampedTransform tag1_cam;
+    tl.lookupTransform("camera_raw", "tag_0", ros::Time(0), tag0_cam);
+    tl.lookupTransform("camera_raw", "tag_1", ros::Time(0), tag1_cam);
+    cam_tag0 = tag0_cam.inverse();
+    cam_tag1 = tag1_cam.inverse();
+    // cam_tag0_vec = cam_tag0*cam_tag0.getOrigin();
+    return cam_tag0;
+    // tl.lookupTransform("camera", "tag_1", ros::Time(0), tag1_cam);
 
-  try {
-    tl.lookupTransform("camera", "tag_0", ros::Time(0), tag0_cam);
-    tl.lookupTransform("camera", "tag_1", ros::Time(0), tag1_cam);
-    tf::Vector3 tag0_cam_vec = tag0_cam.getOrigin();
-    tf::Vector3 tag1_cam_vec = tag1_cam.getOrigin();
+    // tf::Vector3 tag0_cam_vec = tag0_cam.getOrigin();
+    // tf::Vector3 tag0_origin = tag0.getOrigin();
+    // tf::Vector3 tag1_cam_vec = tag1_cam.getOrigin();
 
-    double fused_x = (0.5*(tag0.getX() + tag0_cam_vec.x()) +
-                      0.5*(tag1.getX() + tag1_cam_vec.x()));
-    double fused_y = (0.5*(tag0.getY() + tag0_cam_vec.y()) +
-                      0.5*(tag1.getY() + tag1_cam_vec.y()));
-    double fused_z = (0.5*(tag0.getZ() + tag0_cam_vec.z()) +
-                      0.5*(tag1.getZ() + tag1_cam_vec.z()));
-    return tf::Vector3(fused_x, fused_y, fused_z);
+    // double fused_x = (tag0_origin.getX() + tag0_cam.x());
+    // double fused_y = (tag0_origin.getY() + tag0_cam.y());
+    // double fused_z = (tag0_origin.getZ() + tag0_cam.z());
+    // return tf::Vector3(fused_x, fused_y, fused_z);
 
-  } catch(tf::TransformException ex) {
-    return tf::Vector3(0,0,0);  //TODO: Implement better error handling!
-  }
 }
 
 int main(int argc, char** argv) {
@@ -57,15 +57,17 @@ int main(int argc, char** argv) {
 
   while (nh.ok()) {
     ros::Time cur_time = ros::Time::now();
-    tf::Vector3 cam_pose = get_cam_pose(tag_listener);
-    cam_pose_g.setOrigin(cam_pose);
+    // tf::Vector3 cam_pose = get_cam_pose(tag_listener, tag0_pose_g, tag1_pose_g);
+    // cam_pose_g.setOrigin(cam_pose);
+    cam_pose_g = get_cam_pose(tag_listener, tag0_pose_g, tag1_pose_g);
 
-    //transform_br.sendTransform(
-    //  tf::StampedTransform(cam_pose, cur_time, "world", "camera"));
     transform_br.sendTransform(
-      tf::StampedTransform(tag0_pose_g, cur_time, "world", "tag_0"));
+      tf::StampedTransform(cam_pose_g, cur_time, "tag_0_g", "camera"));
     transform_br.sendTransform(
-      tf::StampedTransform(tag1_pose_g, cur_time, "world", "tag_1"));
+      tf::StampedTransform(tag0_pose_g, cur_time, "world", "tag_0_g"));
+    transform_br.sendTransform(
+      tf::StampedTransform(tag1_pose_g, cur_time, "world", "tag_1_g"));
+
     rate.sleep();
     ros::spinOnce();
   }
